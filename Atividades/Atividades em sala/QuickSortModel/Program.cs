e4abc0c58845e991cfcd54019400f8a2e842bec7
João Vitor <darosa.joao@yahoo.com.br>
2025-10-25 17:41:07 -0300

    Aula 22/10/2025

---
+++ Atividades/Atividades em sala/QuickSortModel/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 5, 2, 9, 1, 5, 6 };
+Console.WriteLine("Vetor a ser ordenado:");
+Console.WriteLine(string.Join(", ", array));
+QuickSortModel.QuickSort.Sort(array);
+Console.WriteLine("Vetor ordenado:");
+Console.WriteLine(string.Join(", ", array));
+
+Console.WriteLine("Vetor de nomes:");
+string[] stringArray = { "Índice fora dos limites", "Estouro de pilha", "Memória insuficiente", "Exceção de ponteiro nulo", "Referência nula" };
+QuickSortModel.QuickSort.Sort(stringArray);
+Console.Write(string.Join(",\n", stringArray.Select(s => $"- {s}")));
